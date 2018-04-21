2e01527f6119cffbeb4cb966ccaa272c42dda7b2
akat32 <baduk32127@gmail.com>
2018-04-21 22:22:39 +0900

    fix routing

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
-module.exports = (express, fs, path)=>{
+module.exports = (express, fs, path,io)=>{
   let router = express.Router();
-  
   let auth = require('./models/auth')(express.Router(),fs,path);
-  let game = require('./models/game')(express.Router(),fs,path);
-
+  let game = require('./models/game')(express.Router(),io);
   router.use('/game', game);
   router.use('/auth', auth);
-  
+
+
+
+  //test
+  // console.log('router : ', router);
+  // console.log('express.Router() : ', express.Router());
+  // if(router === express.Router()) console.log('T')
+  // else console.log('F')
+
   return router;
 }
